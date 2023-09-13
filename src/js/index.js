dc43cd4703586c68c152b8a926e548196df48491
OksanaMosk <oks-m3@ukr.net>
2023-09-14 01:31:22 +0300

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,78 @@
+import { BASE_URL, API_KEY } from './constants';
+
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { itemSearch } from '../js/constants';
 
 const inputEl = document.querySelector('.searchQuery');
 const searchEl = document.querySelector('button');
-const divGallery = document.querySelector('.gallery');
 
-searchEl.addEventListener('click', handlerSearh);
+const photoListEl = document.querySelector('.movie-list');
+const loadMoreEl = document.querySelector('.load-more');
+
+let page = 1;
+
+const renderList = (arr, container) => {
+  const markup = arr.map(
+      (item) => `<li>
+  <div class = "photo-card">
+  <img src = ${item.webformatURL} alt = ${item.tags}
+          loading = "lazy"
+          width = "400"
+        />
+        <div clas = "info">
+          <p class = "info-item">
+            <b>Likes ${item.likes}</b>
+          </p>
+          <p class = "info-item">
+            <b>Views ${item.views}</b>
+          </p>
+          <p class = "info-item">
+            <b>Comments ${item.comments}</b>
+          </p>
+          <p class = "info-item">
+            <b>Downloads ${item.downloads}</b>
+          </p>
+        </div>
+      </div>
+    </li>`
+    )
+    .join();
+
+  container.insertAdjacentHTML('beforeend', markup);
+};
+
+const fetchItem = () => {
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: '',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    page,
+    per_page: 40,
+  });
+  const endpoint = BASE_URL + '/?' + params.toString();
+
+  return fetch(endpoint).then(res => {
+    if (res.status === 200) {
+      return res.json();
+    }
+    throw new Error(res.statusText);
+  });
+};
+
+const loadMoreHandler = () => {
+  page += 1;
+
+  fetchItem().then(data => console.log(data));
+};
+
+fetchItem().then(res => renderList(res.results, photoListEl));
+
+loadMoreEl.addEventListener('click', loadMoreHandler);
+
+// searchEl.addEventListener('click', handlerSearh);
 
 // const technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node'];
 // const list = document.querySelector('.list');
@@ -16,56 +81,30 @@ searchEl.addEventListener('click', handlerSearh);
 //   .map(technology => `<li class="list-item">${technology}</li>`)
 //   .join('');
 
-const arrayList = []
-const everyItem = arrayList.tags
+// const arrayList = [];
+// const everyItem = arrayList.tags;
 
-function handlerSearh(e) {
-  e.preventDefault();
-function functionSearh() {
-  if (arrayList.tags.includes(itemSearch.tags)) {
-    arrayList.filter(itemSearch);
-        divGallery.innerHTML = `<ul><li>
-      <div class = "photo-card">
-        <img
-          src = "${webformatURL}"
-          alt = "${tags[0].name}"
-          loading = "lazy"
-          width = "400"
-        />
-        <div clas = "info">
-          <p class = "info-item">
-            <b>Likes ${likes}</b>
-          </p>
-          <p class = "info-item">
-            <b>Views ${views}</b>
-          </p>
-          <p class = "info-item">
-            <b>Comments ${comments}</b>
-          </p>
-          <p class = "info-item">
-            <b>Downloads ${downloads}</b>
-          </p>
-        </div>
-      </div>
-    </li>
-      </ul>`
-  }
-const itemSearch = e.currentTarget.value;
-  fetchItem(itemSearch)
-    .then(functionSearh)
-    console.log (everyItem(tags))
-    .catch(error => {
-      messageError();
-    });
-}
+// function handlerSearh(e) {
+//   e.preventDefault();
+//   function functionSearh() {
+//     if (arrayList.tags.includes(itemSearch.tags)) {
+//       arrayList.filter(itemSearch);
+//       divGallery.innerHTML = `<ul>
+//       </ul>`;
+//     }
+//     const itemSearch = e.currentTarget.value;
+//     fetchItem(itemSearch).then(functionSearh);
+//     console.log(everyItem(tags)).catch(error => {
+//       messageError();
+//     });
+//   }
 
-
-function messageError() {
-  Notiflix.Notify.failure(
-    'Oops! Something went wrong! Try reloading the page!',
-    {
-      timeout: 2000,
-    }
-  );
-}
-}
+//   function messageError() {
+//     Notiflix.Notify.failure(
+//       'Oops! Something went wrong! Try reloading the page!',
+//       {
+//         timeout: 2000,
+//       }
+//     );
+//   }
+// }
